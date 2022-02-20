1bb2b8b8bfe8b2cb58913c5fa9cb3f6fa47542f4
Agustín Ollín <85704884+agustin-ollin@users.noreply.github.com>
2022-02-19 19:44:21 -0600

    Ejercicio 16 - Avance

---
+++ Ejercicios/EJ 16/operaciones.js
@@ -0,0 +1,7 @@
+/**
+ * 16. Ejercicio con DOM y eventos
+ * -Formulario campos: Nombre, Apellidos y edad
+ * -Botón de enviar el formulario: eventos submit
+ * -Mostrar datos por pantalla
+ * -Validación del formulario para que no metan datos vacíos
+ */
